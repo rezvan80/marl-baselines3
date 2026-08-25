30d3741aee56f908f725cb9a9746076488991529
rezvan80 <rbeheshtinezhad80@gmail.com>
2026-08-25 13:46:52 +0330

    Update main.py

--- marl_baselines3/main.py
+++ marl_baselines3/main.py
@@ -8,21 +8,216 @@ from cityflow_env import CityFlowEnv
 from independent_ppo import PPO
 import shutil
 import torch as th
+import random
+import numpy as np
 import torch.nn as nn
 from typing import Tuple
 from stable_baselines3.common.policies import ActorCriticPolicy
 import math
 import torch.nn.functional as F
+import numpy as np
+from evaluate_policy import evaluate_policy
+seed = 42
+np.random.seed(seed)
+th.manual_seed(seed)
+random.seed(seed)
+
+def lane_relation(phase_map):
+    num_lanes=24
+    lane_relation = np.zeros((num_lanes, num_lanes))
+
+    for phase_lanes in phase_map:
+        for i in phase_lanes:
+            for j in phase_lanes:
+                
+                lane_relation[i, j] = 1
+    return lane_relation
+  
 def merge(dic_tmp, dic_to_change):
     dic_result = copy.deepcopy(dic_tmp)
     dic_result.update(dic_to_change)
     return dic_result
+phase_map=[[1, 4, 12, 13, 14, 15, 16, 17], [7, 10, 18, 19, 20, 21, 22, 23], [0, 3, 18, 19, 20, 21, 22, 23], [6, 9, 12, 13, 14, 15, 16, 17]],
+relation=lane_relation(phase_map)
+
+class MyMultiHeadAttention(nn.Module):
+
+    def __init__(self, d_model=128, num_heads=4):
+        super().__init__()
+
+        assert d_model % num_heads == 0
+
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.head_dim = d_model // num_heads
+        self.bias=nn.Parameter(th.zeros(num_heads))
+        self.bias2=nn.Parameter(th.zeros(num_heads))
+        self.q_proj = nn.Linear(d_model, d_model)
+        self.k_proj = nn.Linear(d_model, d_model)
+        self.v_proj = nn.Linear(d_model, d_model)
+
+        self.out_proj = nn.Linear(d_model, d_model)
+
+    def forward(self, x, mask=None):
+
+        B, N, D = x.shape
+
+        # --------------------------------
+        # Q, K, V
+        # --------------------------------
+
+        Q = self.q_proj(x)
+        K = self.k_proj(x)
+        V = self.v_proj(x)
+
+        # [B, N, D]
+        #      ↓
+        # [B, H, N, head_dim]
+
+        Q = Q.view(B, N, self.num_heads, self.head_dim)
+        K = K.view(B, N, self.num_heads, self.head_dim)
+        V = V.view(B, N, self.num_heads, self.head_dim)
+
+        Q = Q.transpose(1, 2)
+        K = K.transpose(1, 2)
+        V = V.transpose(1, 2)
+
+        # [B, H, N, head_dim]
+
+        # --------------------------------
+        # Attention scores
+        # --------------------------------
+
+        scores = th.matmul(
+            Q,
+            K.transpose(-2, -1)
+        )
+        phase_map=[[1, 4, 12, 13, 14, 15, 16, 17], [7, 10, 18, 19, 20, 21, 22, 23], [0, 3, 18, 19, 20, 21, 22, 23], [6, 9, 12, 13, 14, 15, 16, 17]],
+        relation=lane_relation(phase_map)
+        
+        relation = th.as_tensor(relation,dtype=scores.dtype,device=scores.device)
+        relation=relation.repeat_interleave(10, dim=0).repeat_interleave(10, dim=1)
+        I=th.eye(24)
+        I=I.repeat_interleave(10, dim=0).repeat_interleave(10, dim=1)
+        I = th.as_tensor(I,dtype=scores.dtype,device=scores.device)
+        scores = scores / (self.head_dim ** 0.5) + self.bias[None, :, None, None]*relation[None , None , : , :]+self.bias2[None, :, None, None]*I[None , None , : , :]
+
+        # [B, H, N, N]
+
+        # --------------------------------
+        # Mask
+        # --------------------------------
+
+        if mask is not None:
+            scores = scores.masked_fill(
+                mask[:, None, None, :],
+                float("-inf")
+            )
+
+        # --------------------------------
+        # Softmax
+        # --------------------------------
+
+        attention = th.softmax(
+            scores,
+            dim=-1
+        )
+
+        # --------------------------------
+        # Weighted sum
+        # --------------------------------
+
+        output = th.matmul(
+            attention,
+            V
+        )
+
+        # [B, H, N, head_dim]
+
+        # --------------------------------
+        # Combine heads
+        # --------------------------------
+
+        output = output.transpose(1, 2)
+
+        output = output.contiguous().view(
+            B, N, self.d_model
+        )
+
+        output = self.out_proj(output)
+
+        return output, attention
+class TransformerEncoderBlock(nn.Module):
+
+    def __init__(
+        self,
+        d_model=128,
+        num_heads=4,
+        dim_feedforward=256,
+        dropout=0.1
+    ):
+        super().__init__()
+
+        self.attention = MyMultiHeadAttention(
+            d_model=d_model,
+            num_heads=num_heads
+        )
+
+        self.norm1 = nn.LayerNorm(d_model)
+
+        self.ffn = nn.Sequential(
+            nn.Linear(d_model, dim_feedforward),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(dim_feedforward, d_model)
+        )
+
+        self.norm2 = nn.LayerNorm(d_model)
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, mask=None):
+
+        # Attention
+        attn_out, attention = self.attention(
+            x,
+            mask
+        )
+
+        x = self.norm1(
+            x + self.dropout(attn_out)
+        )
+
+        # Feed-forward
+        ff_out = self.ffn(x)
+
+        x = self.norm2(
+            x + self.dropout(ff_out)
+        )
+
+        return x, attention
 class SelfAttention(nn.Module):
     def __init__(self, d, d_q, d_k, d_v):
         super(SelfAttention, self).__init__()
         self.latent_dim_pi = 4
         self.latent_dim_vf = 4
 
+        self.transformerblock1=TransformerEncoderBlock(d_model=32,
+        num_heads=4,
+        dim_feedforward=64,
+        dropout=0)
+        self.transformerblock2=TransformerEncoderBlock(d_model=32,
+        num_heads=4,
+        dim_feedforward=64,
+        dropout=0)
+        self.transformerblock3=TransformerEncoderBlock(d_model=32,
+        num_heads=4,
+        dim_feedforward=64,
+        dropout=0)
+        self.transformerblock4=TransformerEncoderBlock(d_model=32,
+        num_heads=4,
+        dim_feedforward=64,
+        dropout=0)
 
         self.transformer = nn.TransformerEncoder(
             nn.TransformerEncoderLayer(d_model=32,dim_feedforward=64, nhead=4, dropout=0, batch_first=True),
@@ -40,8 +235,8 @@ class SelfAttention(nn.Module):
             batch_first=True
         )
 
-        self.linear1 = nn.Linear(3, 32)
-        self.linear2 = nn.Linear(3, 32)
+        self.linear1 = nn.Linear(2, 32)
+        self.linear2 = nn.Linear(2, 32)
         self.linear3 = nn.Linear(32, 20)
         self.linear4 = nn.Linear(32, 20)
         self.linear5 = nn.Linear(20, 20)
@@ -72,24 +267,28 @@ class SelfAttention(nn.Module):
 
     def forward(self, x: th.Tensor) -> Tuple[th.Tensor, th.Tensor]:
 
+
         
         #context_vector2=self.policy_net2(context_vector2)
         return self.forward_actor(x),self.forward_critic(x)
 
 
     def forward_actor(self, x: th.Tensor) -> th.Tensor:
-        print(x)
-        x[: , : , 39]=0
-        mask=x[: , : , 30:].reshape(-1, 240)
-        x=x[: , : , :30].reshape(-1, 240, 3)
+        x[: , : ,39]=0
+        mask=x[: , : , 30:].reshape(-1 , 240 ).bool()
+        x=x[: , : , :20].reshape(-1 , 240 , 2) 
         x=self.linear1(x)
-        #x=th.split(x, 24 , dim=1)
-        x=self.transformer(x , src_key_padding_mask=mask)
+    
+        x , _=self.transformerblock1(x , mask)
+        x, _=self.transformerblock2(x , mask)        
+        
+
         x=x.reshape(-1, 24, 10 , 32)
+        mask=mask.reshape(-1, 24, 10 ).unsqueeze(-1)
         phase_feats_map_1=[]
         for i in range(4):
           
-          tmp_feat_1 = x[:, self.phase_map[i], : , :]
+          tmp_feat_1 = x[:, self.phase_map[i], : , :]*(~mask[:, self.phase_map[i], : , :])
           tmp_feat_1_mean = tmp_feat_1.mean(dim=(1, 2),keepdim=True).squeeze(1)
 
           phase_feats_map_1.append(tmp_feat_1_mean)
@@ -108,15 +307,19 @@ class SelfAttention(nn.Module):
 
 
     def forward_critic(self, x: th.Tensor) -> th.Tensor:
-        x[: , : , 39]=0
-        mask=x[: , : , 30:].reshape(-1, 240)
-        x=x[: , : , :30].reshape(-1, 240, 3)
-        x=self.linear2(x) 
-        x=self.transformer2(x , src_key_padding_mask=mask)
+        x[: , : ,39]=0
+        mask=x[: , : , 30:].reshape(-1 , 240 ).bool()
+        x=x[: , : , :20].reshape(-1 , 240 , 2)
+        x=self.linear2(x)
+        x , _=self.transformerblock3(x , mask)
+        x , _=self.transformerblock4(x , mask)
+
         x=x.reshape(-1, 24, 10 , 32)
+        mask=mask.reshape(-1, 24, 10 ).unsqueeze(-1)
+
         phase_feats_map_1=[]
         for i in range(4):
-          tmp_feat_1 = x[:, self.phase_map[i], : , :]
+          tmp_feat_1 = x[:, self.phase_map[i], : , :]*(~mask[:, self.phase_map[i], : , :])
           tmp_feat_1_mean = tmp_feat_1.mean(dim=(1, 2),keepdim=True).squeeze(1)
           phase_feats_map_1.append(tmp_feat_1_mean)
               
@@ -125,15 +328,10 @@ class SelfAttention(nn.Module):
         hidden=self.Relu2(hidden)
         hidden=self.linear6(hidden)
         hidden=self.Relu4(hidden)
-        hidden=self.linear8(hidden)                 
-
-
-        
-
-           
-   
+        hidden=self.linear8(hidden) 
         
         return hidden.reshape(-1 , 4)
+
 
 
 
@@ -278,8 +476,10 @@ def main(in_args=None):
     
     env=CityFlowEnv(deploy_dic_path["PATH_TO_WORK_DIRECTORY"],deploy_dic_path["PATH_TO_WORK_DIRECTORY"], deploy_dic_traffic_env_conf , deploy_dic_path)
     ppo=PPO(CustomPolicy  ,  env , 12 , verbose=1 , n_steps=120)
-    ppo.learn(env, total_timesteps=120000 , log_interval=1)
+    ppo.learn(env, total_timesteps=24000 , log_interval=1)
 
+    mean , std= evaluate_policy(ppo , env)
+    print(mean , std)
 if __name__ == "__main__":
     args = parse_args()
 
